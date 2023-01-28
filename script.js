8a7d8de41853a1b6f2367c6ca74872e936d4c12b
somehowalive <slow.optimism@gmail.com>
2023-01-28 23:11:07 +0100

    "made the hero background change also"

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const hero = document.querySelector(".hero");
 const illuminateBtn = document.querySelector("#illuminate");
 const moon = document.querySelector(".moon");
 const overlay = document.querySelector(".overlay");
@@ -5,4 +6,8 @@ const overlay = document.querySelector(".overlay");
 illuminateBtn.onclick = () => {
   overlay.classList.add("full-moon");
   moon.classList.add("illuminated");
+  setTimeout(() => {
+    overlay.remove();
+    hero.style.backgroundColor = "rgb(5, 20, 43)";
+  }, 1000);
 };
